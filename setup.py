4c821001c16afd9d3caebb98a8683bb81774568c
Oliver Thomas <ot44@sussex.ac.uk>
2019-03-08 12:36:17 +0000

    trying stuf out

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='testsvm',
+    version='0.1.0',
+    author='O. Thomas',
+    packages=['exsvm'],
+    description='Framework for algorithmic fairness comparisons',
+    python_requires=">=3.6",
+    install_requires=[
+
+    ],
+)
